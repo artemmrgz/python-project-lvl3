1ea2b1a3cc4a78c4b65e3d7d7ce0e7b78407753f
Artem Marhaza <artemmrgz@gmail.com>
2021-06-29 11:05:58 +0200

    add img saving (step2)

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,26 +1,65 @@
 import os
 import re
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
-def download(path_to_dir, url):
-    file_path = create_path(path_to_dir, url)
-    text = get_page_data(url)
-    save_data(file_path, text)
+def download(path_to_dir, page_url):
+    file_path = create_path(path_to_dir, page_url)
+    img_dir = create_img_dir(file_path)
+    html_file = requests.get(page_url).text
+    changed_file = change_links_and_save_imgs(html_file, page_url, img_dir)
+    save_file(file_path, changed_file)
 
 
 def create_path(path_to_dir, url):
-    url_parts = re.split('[^0-9a-zA-z]+', url)
+    url_parts = re.split('[^0-9a-zA-Z]+', url)
     url_parts.pop(0)
-    filename = '-'.join(url_parts) + '.html'
+    filename = '-'.join(url_parts)[:25] + '.html'
     return os.path.join(path_to_dir, filename)
 
 
-def get_page_data(url):
-    page = requests.get(url)
-    return page.text
+def create_img_dir(path_to_file):
+    path_parts = os.path.splitext(path_to_file)
+    dir_ = path_parts[0] + '_files'
+    os.mkdir(dir_)
+    return dir_
 
 
-def save_data(location, page):
-    with open(location, 'w') as f:
+def change_links_and_save_imgs(html, page_url, img_dir):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    for img in soup.find_all('img'):
+        img_url = create_img_url(page_url, img['src'])
+        img_path = create_img_path(img_dir, img_url)
+        save_img(img_path, img_url)
+        img['src'] = img_path
+    print(soup.prettify(formatter="html5"))
+    return soup.prettify(formatter="html5")
+
+
+def create_img_path(img_dir, img_url):
+    without_extension = os.path.splitext(img_url)[0]
+    url_parts = re.split('[^0-9a-zA-Z]+', without_extension)
+    url_parts.pop(0)
+    img_name = '-'.join(url_parts)[:50] + '.png'
+    return os.path.join(img_dir, img_name)
+
+
+def create_img_url(page_url, img_url):
+    parsed_url = urlparse(page_url)
+    parsed_img_url = urlparse(img_url)
+    starting_from_path = parsed_img_url._replace(scheme='', netloc='')
+    img_url = starting_from_path.geturl()
+    return f'{parsed_url[0]}://{parsed_url[1]}{img_url}'
+
+
+def save_img(save_to, img_url):
+    with open(save_to, 'wb') as f:
+        f.write(requests.get(img_url).content)
+
+
+def save_file(save_to, page):
+    with open(save_to, 'w') as f:
         f.write(page)
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,19 +1,61 @@
 import os
 import tempfile
+import pyfakefs
 import requests
 import requests_mock
-from page_loader.page_loader import download, create_path
+import filecmp
+from page_loader.page_loader import create_path, create_img_dir, change_links_and_save_imgs, create_img_path, create_img_url
 
 
 TEST_URL = 'https://ru.hexlet.io/courses'
-TEST_DATA = 'Some data'
+TEST_IMG_NAME = '/assets/professions/nodejs.png'
+TEST_HTML_NAME = 'ru-hexlet-io-courses.html'
+IMG_DIR = 'ru-hexlet-io-courses_files'
+TEST_IMG_URL = 'https://ru.hexlet.io/derivations/image/fill_webp/540/320/eyJpZCI6Ijk4YjY4ODdjMjVlOTI1OWVjOWQ0MDFkNzc3O' \
+               'TEwNWM4LnBuZyIsInN0b3JhZ2UiOiJzdG9yZSJ9?signature=8b9d21429f1b423628e7a9c4521839809652b4a7f05db0bc83d4' \
+               '5b6bf2658d14'
 
 
-def test_download():
+def locate(file):
+    return os.path.join('tests', 'fixtures', file)
+
+
+def test_create_path():
     with tempfile.TemporaryDirectory() as tmpdir:
-        with requests_mock.Mocker() as m:
-            m.get(TEST_URL, text=TEST_DATA)
-            download(tmpdir, TEST_URL)
-        with open(create_path(tmpdir, TEST_URL), 'r') as f:
-            output = f.read()
-    assert output == TEST_DATA
+        expected = os.path.join(tmpdir, 'ru-hexlet-io-courses.html')
+        output = create_path(tmpdir, TEST_URL)
+        assert output == expected
+
+
+def test_create_img_dir(fs):
+    assert IMG_DIR == create_img_dir(TEST_HTML_NAME)
+
+
+def test_change_links_and_save_imgs(fs):
+    fs.create_dir(IMG_DIR)
+    fs.add_real_directory(os.path.join('tests', 'fixtures'))
+    with open(locate('test.html')) as f:
+        input_html = f.read()
+    with open(locate('expected.bin'), 'rb') as file:
+        expected_img = file.read()
+    with requests_mock.Mocker() as m:
+        m.get(TEST_IMG_URL, content=expected_img)
+        output_html = change_links_and_save_imgs(input_html, TEST_URL, IMG_DIR)
+    img_file = os.path.join(IMG_DIR, 'ru-hexlet-io-derivations-image-fill-webp-540-320-e.png')
+    with open(locate('expected.html')) as f:
+            expected = f.read()
+    assert output_html == expected
+    assert filecmp.cmp(locate('expected.bin'), img_file)
+    assert filecmp.cmp(locate('expected.bin'), img_file, shallow=False)
+
+
+def test_create_img_path():
+    expected = 'ru-hexlet-io-courses_files/ru-hexlet-io-derivations-image-fill-webp-540-320-e.png'
+    output = create_img_path(IMG_DIR, TEST_IMG_URL)
+    assert expected == output
+
+
+def test_create_img_url():
+    expected = 'https://ru.hexlet.io/assets/professions/nodejs.png'
+    output = create_img_url(TEST_URL, TEST_IMG_NAME)
+    assert expected == output
