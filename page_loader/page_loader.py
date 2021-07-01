fec61f9ee0495caeedba58611da1f562892da79a
Artem Marhaza <artemmrgz@gmail.com>
2021-07-01 16:42:53 +0200

    add logging

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -3,6 +3,10 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
 
 
 tags = {'img': 'src', 'link': 'href', 'script': 'src'}
@@ -12,8 +16,10 @@ MAX_RESOURCE_NAME_LENGTH = 50
 
 def download(path_to_dir, base_url):
     file_path = create_file_path(path_to_dir, base_url)
+    logging.debug(f'File path - {file_path}')
     files_dir = create_dir(file_path)
     html_file = requests.get(base_url).text
+    logging.debug(f'Get request to {base_url}')
     assets, changed_file = prepare_assets(html_file, base_url, files_dir)
     save_file(file_path, changed_file)
     save_resources(files_dir, assets)
@@ -29,6 +35,8 @@ def create_file_path(path_to_dir, base_url):
 def create_dir(path_to_file):
     path_parts = os.path.splitext(path_to_file)
     dir_ = path_parts[0] + '_files'
+    logging.warning(f'dir {dir_} is about to be created,\
+                    dir with such name shouldn\'t exist')
     os.mkdir(dir_)
     return dir_
 
@@ -40,8 +48,10 @@ def prepare_assets(html, base_url, files_dir):
     for resource in resources:
         tag = tags[resource.name]
         resource_url = create_resource_url(base_url, resource.get(tag))
+        logging.debug(f'Resource url - {resource_url}')
         resource_path = os.path.join(files_dir,
                                      create_resource_path(resource_url))
+        logging.debug(f'Resource path - {resource_path}')
         assets.append((resource_url, resource_path))
         resource[tag] = resource_path
     return assets, soup.prettify(formatter="html5")
